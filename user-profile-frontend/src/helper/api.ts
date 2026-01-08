8f8f39b5370c5e7ad6e8d7863b824128dadba2c6
Mert Can Özserçe <97916389+McannOzserce@users.noreply.github.com>
2026-01-08 22:04:38 +0300

    Change API base URL to production server

--- user-profile-frontend/src/helper/api.ts
+++ user-profile-frontend/src/helper/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000", // Sakın sonuna /profiles veya / ekleme
+  baseURL: "https://webprojesi.onrender.com", // Sakın sonuna /profiles veya / ekleme
   headers: {
     "Content-Type": "application/json",
   },
